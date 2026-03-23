281f6d3750cb857de8b84c27e71ece62092546bd
Enrique Orona <alfredo9709@gmail.com>
2026-03-23 03:20:13 -0600

    config.js automatic detection for localhost and production

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
-const API = 'https://pos-schoolshop-api-production.up.railway.app';
+//const API = 'https://pos-schoolshop-api-production.up.railway.app';
+//const API = 'localhost:8080';
+const API = window.location.hostname === 'localhost'
+  ? 'http://localhost:8080'
+  : 'https://pos-schoolshop-api-production.up.railway.app';
